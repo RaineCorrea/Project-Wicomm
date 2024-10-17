8351626b86a96e50944322a462660f8f1a156001
unknown <raineenf@gmail.com>
2024-10-17 17:17:51 -0300

    feature(home): correction img

--- src/mocks/bannerVideo.ts
+++ src/mocks/bannerVideo.ts
@@ -5,7 +5,7 @@ export const bannerVideo = [
         Arranhar & Relaxar`,
     subTitle:
       "Hammer can low idea of. Lunch able design items needle here stop drive. / or devil pants game close. Prioritize out key view / vendor we've. 4-blocker gave shift ideal based post.",
-    img: "src/components/assets/svg/img-video.svg",
+    img: "/assets/svg/img-video.svg",
     linkGat: "MAIS SOBRE A GAT",
     videoUrl: "https://www.youtube.com/watch?v=EAjQNcH9ug0",
   },
